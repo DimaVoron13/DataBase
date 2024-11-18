332f89140d20fb39ecf7fbadda688298ebd2853b
Дмитрий <dima.voron13666@gmail.com>
2024-11-18 04:56:28 +0300

    1 commit

---
+++ src/main/java/DataBase/hogwards/HogwardsApplication.java
@@ -0,0 +1,13 @@
+package DataBase.hogwards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HogwardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HogwardsApplication.class, args);
+	}
+
+}
